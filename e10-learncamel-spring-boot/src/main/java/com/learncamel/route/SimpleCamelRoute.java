c37e2e0210370a065a117896fdb6dcd47b73a14d
Marcello Semboli <dinogen@gmail.com>
2018-04-04 15:12:19 +0200

    esempio 10

---
+++ e10-learncamel-spring-boot/src/main/java/com/learncamel/route/SimpleCamelRoute.java
@@ -0,0 +1,27 @@
+package com.learncamel.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by z001qgd on 1/24/18.
+ */
+@Component
+public class SimpleCamelRoute  extends RouteBuilder{
+
+    @Autowired
+    Environment environment;
+
+    @Override
+    public void configure() throws Exception {
+
+        from("{{startRoute}}")
+                .log("Timer Invoked and the body" + environment.getProperty("message"))
+                .pollEnrich("{{fromRoute}}")
+                .to("{{toRoute1}}");
+
+
+    }
+}
