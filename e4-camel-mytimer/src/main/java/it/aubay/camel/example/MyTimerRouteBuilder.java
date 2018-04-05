962acbf2559937b0184a74e07dadc8e585c10c41
Marcello Semboli <dinogen@gmail.com>
2018-04-05 15:17:20 +0200

    piccoli aggiustamenti

--- e4-camel-mytimer/src/main/java/it/aubay/camel/example/MyTimerRouteBuilder.java
+++ e4-camel-mytimer/src/main/java/it/aubay/camel/example/MyTimerRouteBuilder.java
@@ -5,7 +5,7 @@ public class MyTimerRouteBuilder extends RouteBuilder {
 	Integer counter = 0;
 	@Override
 	public void configure() throws Exception {
-		from("timer://myTimer?period=2000")
+		from("timer://myTimer?period=2000") // il timer scatta ogni 2000 millisecondi
 		.setBody()		   
 		.simple("Hello World Camel fired at ${header.firedTime}")		   
 		.to("stream:out");
